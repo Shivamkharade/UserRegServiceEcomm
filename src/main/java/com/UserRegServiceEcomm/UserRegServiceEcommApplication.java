9bf92e8a5381374a6ca4d0e134b3aed5b733567d
Shivamkharade <shivamkharade189@gmail.com>
2026-04-23 12:02:32 +0530

    first commit

---
+++ src/main/java/com/UserRegServiceEcomm/UserRegServiceEcommApplication.java
@@ -0,0 +1,13 @@
+package com.UserRegServiceEcomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegServiceEcommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegServiceEcommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UserRegServiceEcomm/UserRegServiceEcommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UserRegServiceEcomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegServiceEcommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
